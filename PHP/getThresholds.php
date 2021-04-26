60989ea4fa0b31f0bca6f24920e9bdc59f8354cd
Oscar Höfenstock <7043@htl.rennweg.at>
2021-04-26 17:30:57 +0200

    added: getThresholds.php, with token-authentication

--- PHP/getThresholds.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "session.php";
-
-$thresholds = $conn->prepare("SELECT `co2`, `humidity`, `temperature` FROM `threshold` WHERE `pk_threshold_id` = 1");
-$thresholds->execute();
-echo json_encode($thresholds->fetch(PDO::FETCH_ASSOC));
