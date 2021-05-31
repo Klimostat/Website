12d06ad427b7bdaf73d7e059ca625847d31578e3
Florentin <florentin.schaefer@gmail.com>
2021-05-31 16:17:35 +0200

    fix thresholds

--- Database/20_21_KLS_sql_creates.sql
+++ Database/20_21_KLS_sql_creates.sql
@@ -8,12 +8,10 @@ DROP TABLE IF EXISTS `station`;
 CREATE TABLE IF NOT EXISTS `station` (
     `pk_station_id` INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(32) NOT NULL,
-    `alert_message_humidity` TEXT NOT NULL DEFAULT 'Humidity too high.',
-    `alert_message_co2` TEXT NOT NULL DEFAULT 'CO2 concentration too high.',
-    `alert_message_temp` TEXT NOT NULL DEFAULT 'Temperature too high.',
     `threshold_co2` FLOAT NOT NULL,
     `threshold_humidity` FLOAT NOT NULL,
-    `threshold_temperature` FLOAT NOT NULL,
+    `alert_message_co2` TEXT NOT NULL DEFAULT 'CO2 concentration too high',
+    `alert_message_humidity` TEXT NOT NULL DEFAULT 'Humidity too low',
     `location` TEXT NOT NULL,
     `token` TEXT NOT NULL,
     `last_connection` TIMESTAMP NULL
@@ -70,16 +68,16 @@ INSERT INTO `user` (`pk_user_id`, `username`, `password_hash`)
 VALUES (0, 'admin', '$argon2id$v=19$m=65536,t=4,p=1$SjNLQldCS0FLTGx1YTV2Vg$nFQ9uLFlD7Bu8iyBw0sd8ai923Z2CpwPSc7s3ErjbVo'),
     (0, 'root', '$argon2id$v=19$m=65536,t=4,p=1$UlV0enVtOTZ3YlpSWEFHbg$GStLB+YkIJ8XVSojMq14m5HT/7lgfnSzYssIIXdJcgg');
 
-INSERT INTO `station` (`name`, `location`, `token`, `threshold_co2`, `threshold_humidity`, `threshold_temperature`)
+INSERT INTO `station` (`name`, `location`, `token`, `threshold_co2`, `threshold_humidity`)
 VALUE
-    ('test_station', 'mobil', '$argon2id$v=19$m=65536,t=4,p=1$UVVzSEdINGNIUWJpZUN4cQ$wFH1+6ztyX8xBRtOSbW7pjZ4SujcezIrVLB0v38I/Q4', 2000, 50, 30),
-    ('Virt2', 'In der Datenbank', 'token2', 2000, 50, 30),
-    ('Virt3', 'In der Datenbank', 'token3', 2000, 50, 30),
-    ('Virt4', 'In der Datenbank', 'token4', 2000, 50, 30),
-    ('Virt5', 'In der Datenbank', 'token5', 2000, 50, 30),
-    ('Virt6', 'In der Datenbank', 'token6', 2000, 50, 30),
-    ('Virt7', 'In der Datenbank', 'token7', 2000, 50, 30),
-    ('Virt8', 'In der Datenbank', 'token8', 2000, 50, 30),
-    ('Virt9', 'In der Datenbank', 'token9', 2000, 50, 30),
-    ('Virt10', 'In der Datenbank', 'token10', 2000, 50, 30),
-    ('Virt11', 'In der Datenbank', 'token11', 2000, 50, 30);
+    ('test_station', 'mobil', '$argon2id$v=19$m=65536,t=4,p=1$UVVzSEdINGNIUWJpZUN4cQ$wFH1+6ztyX8xBRtOSbW7pjZ4SujcezIrVLB0v38I/Q4', 2000, 30),
+    ('Virt2', 'In der Datenbank', 'token2', 2000, 30),
+    ('Virt3', 'In der Datenbank', 'token3', 2000, 30),
+    ('Virt4', 'In der Datenbank', 'token4', 2000, 30),
+    ('Virt5', 'In der Datenbank', 'token5', 2000, 30),
+    ('Virt6', 'In der Datenbank', 'token6', 2000, 30),
+    ('Virt7', 'In der Datenbank', 'token7', 2000, 30),
+    ('Virt8', 'In der Datenbank', 'token8', 2000, 30),
+    ('Virt9', 'In der Datenbank', 'token9', 2000, 30),
+    ('Virt10', 'In der Datenbank', 'token10', 2000, 30),
+    ('Virt11', 'In der Datenbank', 'token11', 2000, 30);
