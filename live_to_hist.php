303b74feb393e7035a5627c362ad3f949af33c38
Ketamine-maker <75989932+Ketamine-maker@users.noreply.github.com>
2021-06-06 17:26:39 +0200

    ein paar Kleinigkeiten hinzugefügt
    1) session hinzugefügt
    2) 2 Tage alte Daten werden in hist_data automatisch inserted
    3) ein paar debug Ausgaben wurden hinzugefügt

--- live_to_hist.php
+++ live_to_hist.php
@@ -1,35 +1,54 @@
 <?php
 error_reporting(E_ALL);
-$db = new PDO("mysql:host=localhost;dbname=klimostat", "root", "");
+include "session.php";
 
-$fk_station_id = 0;
-while ($fk_station_id <= 4){
-    $fk_station_id++;
+$stations = $conn->prepare("select pk_station_id from station;");
+$stations->execute();
 
-    $from = "2020-05-10 00:00:00";
-    $to = "2022-05-11 00:00:00";
-    $data = $db->prepare("
+echo "<br> Connection to Database established";
+
+while ($station = $stations -> fetch(PDO::FETCH_ASSOC)) {
+
+    echo "<br> begin of while loop";
+    echo "<br>";
+
+    $fk_station_id = $station['pk_station_id'];
+
+    $today = date("Y-m-d H:i:s");
+    $two_days_ago = date("Y-m-d H:i:s", strtotime("-2 day"));
+
+    $data = $conn ->prepare("
     select timestamp(concat(left(l.pk_measurement_time, 17), '00')) time1, max(l.co2) max_co2, min(l.co2) min_co2, max(l.humidity) max_humidity, min(l.humidity) min_humidity, max(l.temperature) max_temperature, min(l.temperature) min_temperature from live_data l
-    where l.pk_measurement_time > :from
-    and l.pk_measurement_time <= :to
+    where l.pk_measurement_time <= :to
     and l.fk_station_id = :fk_station_id
     group by time1;
     ");
-    echo "lol";
+
+    echo "<br> Live Data selected";
+    echo "<br>";
+
     $data->bindParam(":fk_station_id", $fk_station_id);
-    $data->bindParam(":from", $from);
-    $data->bindParam(":to", $to);
+    $data->bindParam(":to", $two_days_ago);
     $data->execute();
 
-    while ($zeile = $data->fetch(PDO::FETCH_ASSOC)) {
-        echo $min_co2 = $zeile['min_co2'];
-        echo $max_co2 = $zeile['max_co2'];
-        echo $min_humidity = $zeile['min_humidity'];
-        echo $max_humidity = $zeile['max_humidity'];
-        echo $min_temperature = $zeile['min_temperature'];
-        echo $max_temperature = $zeile['max_temperature'];
-        echo $time = $zeile['time1'];
-        $insert_to_historical = $db->prepare("INSERT INTO historical_data (`fk_station_id`, `pk_measurement_time`, `min_co2`, `max_co2`, `min_humidity`, `max_humidity`, `min_temperature`, `max_temperature`) VALUES (:fk_station_id, :time1, :min_co2, :max_co2, :min_humidity, :max_humidity, :min_temperature, :max_temperature)");
+    while ($line = $data -> fetch(PDO::FETCH_ASSOC)) {
+        echo "<br> min co2 value";
+        echo $min_co2 = $line['min_co2'];
+        echo "<br> max co2 value";
+        echo $max_co2 = $line['max_co2'];
+        echo "<br> min humidity value";
+        echo $min_humidity = $line['min_humidity'];
+        echo "<br> max humidity value";
+        echo $max_humidity = $line['max_humidity'];
+        echo "<br> min temperature value";
+        echo $min_temperature = $line['min_temperature'];
+        echo "<br> max temperature value";
+        echo $max_temperature = $line['max_temperature'];
+        echo "<br> timestamp";
+        echo $time = $line['time1'];
+        echo "<br>";
+
+        $insert_to_historical = $conn->prepare("INSERT INTO historical_data (`fk_station_id`, `pk_measurement_time`, `min_co2`, `max_co2`, `min_humidity`, `max_humidity`, `min_temperature`, `max_temperature`) VALUES (:fk_station_id, :time1, :min_co2, :max_co2, :min_humidity, :max_humidity, :min_temperature, :max_temperature)");
         $insert_to_historical->bindParam(":min_co2", $min_co2);
         $insert_to_historical->bindParam(":max_co2", $max_co2);
         $insert_to_historical->bindParam(":min_humidity", $min_humidity);
@@ -39,6 +58,9 @@ while ($fk_station_id <= 4){
         $insert_to_historical->bindParam(":time1", $time);
         $insert_to_historical->bindParam(":fk_station_id", $fk_station_id);
         $insert_to_historical->execute();
+
+        echo "<br> Insert into historical data successful";
+        echo "<br>";
     }
 }
 ?>
