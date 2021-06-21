95528be227f8bf4ce140bfa5b69d763bbf71b703
Florentin Schäfer <florentin.schaefer@gmail.com>
2021-06-21 14:49:55 +0200

    Fix link color im Impressum.php

--- Impressum.php
+++ Impressum.php
@@ -5,14 +5,20 @@
         <meta content="width=device-width, initial-scale=1" name="viewport">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
+        <link href="style_index.css" rel="stylesheet">
         <title>Impressum</title>
+        <style>
+            .mail {
+                color: blue !important;
+            }
+        </style>
     </head>
     <body class="font-sans-serif">
         <div class ="mt-5 ms-5">
             <h1 class="fs-1 fw-bold" > Impressum </h1>
                 <ul class="list-unstyled">
                     <li class="fs-3 mt-5 mb-5 ms-5"> Verantwortlich für Inhalte: Jakob Jakwerth</li>
-                    <li class="fs-3 mt-5 mb-5 ms-5"> Kontakt: <a href="mailto:jakob.jakwerth@htl.rennweg.at">jakob.jakwerth@htl.rennweg.at</a></li>
+                    <li class="fs-3 mt-5 mb-5 ms-5"> Kontakt: <a href="mailto:jakob.jakwerth@htl.rennweg.at" class="mail">jakob.jakwerth@htl.rennweg.at</a></li>
                     <li class="fs-3 mt-5 mb-5 ms-5"> Standort:<br>
                         HTL Wien 3 Rennweg<br>
                         Rennweg 89B<br>
